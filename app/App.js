5a4727dd354a4b2e215e9ecc0f79eede38f4302c
77aram771 <77aram39@gmail.com>
2019-04-08 23:06:57 +0400

    Add Json

--- app/App.js
+++ app/App.js
@@ -1,15 +1,36 @@
 import React from 'react';
+import {View} from 'react-native';
 import {Header} from "./src/components/uikit/Header";
+
+const url = 'https://raw.githubusercontent.com/react-native-village/react-native-init/master/stargate/stargate.json';
 
 export default class App extends React.Component {
 
     state = {
-        title: 'STAR GATE'
+        title: 'STAR GATE',
+        data: null
+    };
+
+    componentDidMount = async () => {
+        try {
+            const response = await fetch(`${url}`);
+            const data = await response.json();
+            this.setState({
+                data: data
+            })
+        } catch (e) {
+            throw e
+        }
+
     };
 
     render() {
+        console.log(this.state.data);
         return (
-            <Header title={this.state.title}/>
+            <View>
+                <Header title={this.state.title}/>
+            </View>
+
         );
     }
 }
