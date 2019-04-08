49994d324a31cfc27fcb04e0da31233d8d866c34
77aram771 <77aram39@gmail.com>
2019-04-08 22:09:11 +0400

    Add New

--- app/App.js
+++ app/App.js
@@ -1,21 +1,35 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <View style={{flex: 1, backgroundColor: 'powderblue'}} />
-        <View style={{flex: 2, backgroundColor: 'skyblue'}} />
-        <View style={{flex: 3, backgroundColor: 'steelblue'}} />
-          <Text>asd</Text>
-      </View>
-    );
-  }
+    render() {
+        console.log('hello');
+
+        return (
+            <View style={styles.container}>
+                <Text style={styles.text}>STAR GATE</Text>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
+    container: {
+        backgroundColor: '#30d0fe',
+        height: 116,
+        justifyContent: 'center',
+        alignItems: 'flex-start',
+        paddingLeft: 22,
+        paddingTop: 71,
+        shadowColor: '#000',
+        shadowOffset: {white: 0, height: 2},
+        shadowOpacity: 0.2,
+        elevation: 2,
+        position: 'relative'
+    },
+    text: {
+        color: 'white',
+        fontSize: 28,
+        fontFamily: 'Roboto',
+    }
 });
